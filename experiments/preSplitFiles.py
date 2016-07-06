4117778a2f33de3c32c7b674ddf73664ad3f951c
Bogdan Vasilescu <vasilescu@gmail.com>
2016-07-06 15:07:45 -0700

    is mini

--- experiments/preSplitFiles.py
+++ experiments/preSplitFiles.py
@@ -20,42 +20,44 @@ reader = UnicodeReader(open('isMinified.csv', 'r'))
 for row in reader:
     isMini[row[0]] = str_to_bool(row[1])
 
-# eligible = [os.path.basename(f) for f in corpus_dir.fullFileNames("*.js")
-#             if not isMini.get(os.path.basename(f), False)]
-# 
-# size = len(eligible)
-# tt = int(0.8 * size)
-# training_size = int(0.9 * tt)
-# tuning_size = int(tt - training_size)
-# testing_size = size - tt
-# 
-# print 'Total:', size
-# print 'Training:', training_size
-# print 'Tuning:', tuning_size
-# print 'Testing:', testing_size
-# 
-# training_sample = random.sample(eligible, training_size)
-# with open('trainingSample.csv', 'wb') as of:
-#     writer = UnicodeWriter(of)
-#     for f in training_sample:
-#         writer.writerow([f])
-# 
-# 
-# tuning_sample = random.sample([f for f in eligible 
-#                                if f not in training_sample], 
-#                               tuning_size)
-# with open('tuningSample.csv', 'wb') as of:
-#     writer = UnicodeWriter(of)
-#     for f in tuning_sample:
-#         writer.writerow([f])
-# 
-# 
-# testing_sample = random.sample([f for f in eligible 
-#                                if f not in training_sample
-#                                and f not in tuning_sample], 
-#                               testing_size)
-# with open('testingSample.csv', 'wb') as of:
-#     writer = UnicodeWriter(of)
-#     for f in testing_sample:
-#         writer.writerow([f])
+
+eligible = [os.path.basename(f) for f in corpus_dir.fullFileNames("*.js")
+            if not isMini.get(os.path.basename(f), False)]
+
+ 
+size = len(eligible)
+tt = int(0.8 * size)
+training_size = int(0.9 * tt)
+tuning_size = int(tt - training_size)
+testing_size = size - tt
+ 
+print 'Total:', size
+print 'Training:', training_size
+print 'Tuning:', tuning_size
+print 'Testing:', testing_size
+ 
+training_sample = random.sample(eligible, training_size)
+with open('trainingSample.csv', 'wb') as of:
+    writer = UnicodeWriter(of)
+    for f in training_sample:
+        writer.writerow([f])
+ 
+ 
+tuning_sample = random.sample([f for f in eligible 
+                               if f not in training_sample], 
+                              tuning_size)
+with open('tuningSample.csv', 'wb') as of:
+    writer = UnicodeWriter(of)
+    for f in tuning_sample:
+        writer.writerow([f])
+ 
+ 
+testing_sample = random.sample([f for f in eligible 
+                               if f not in training_sample
+                               and f not in tuning_sample], 
+                              testing_size)
+with open('testingSample.csv', 'wb') as of:
+    writer = UnicodeWriter(of)
+    for f in testing_sample:
+        writer.writerow([f])
 
