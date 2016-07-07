2d1a1a131d6c03d8111ff5edffc5a6d91180fd89
Bogdan Vasilescu <vasilescu@gmail.com>
2016-07-07 13:12:16 -0700

    debugging

--- experiments/buildLineCorpus.py
+++ experiments/buildLineCorpus.py
@@ -208,37 +208,37 @@ with open(training_sample_path, 'r') as f, \
 
     for result in pool.imap(processFile, reader):
         try:
-            if result[0] is not None:
-                (js_file_path,
-                 orig, 
-                 no_renaming, 
-                 basic_renaming, 
-                 hash_renaming,
-                 hash_def_one_renaming,
-                 hash_def_two_renaming) = result
-              
-                with open(f1, 'a') as f_orig, \
-                        open(f2, 'a') as f_no_renaming, \
-                        open(f3, 'a') as f_basic_renaming, \
-                        open(f4, 'a') as f_hash_renaming, \
-                        open(f5, 'a') as f_hash_def_one_renaming, \
-                        open(f6, 'a') as f_hash_def_two_renaming:
-                    f_orig.writelines(orig)
-                    f_no_renaming.writelines(no_renaming)
-                    f_basic_renaming.writelines(basic_renaming)
-                    f_hash_renaming.writelines(hash_renaming)
-                    f_hash_def_one_renaming.writelines(hash_def_one_renaming)
-                    f_hash_def_two_renaming.writelines(hash_def_two_renaming)
-                
-                writer.writerow([js_file_path, 'OK'])
-    
-            else:
-                writer.writerow(result[1:])
+            (js_file_path,
+             orig, 
+             no_renaming, 
+             basic_renaming, 
+             hash_renaming,
+             hash_def_one_renaming,
+             hash_def_two_renaming) = result
+          
         except Exception, e:
+            writer.writerow(e)
+            continue
+
+        try:
+            with open(f1, 'a') as f_orig, \
+                    open(f2, 'a') as f_no_renaming, \
+                    open(f3, 'a') as f_basic_renaming, \
+                    open(f4, 'a') as f_hash_renaming, \
+                    open(f5, 'a') as f_hash_def_one_renaming, \
+                    open(f6, 'a') as f_hash_def_two_renaming:
+                f_orig.writelines(orig)
+                f_no_renaming.writelines(no_renaming)
+                f_basic_renaming.writelines(basic_renaming)
+                f_hash_renaming.writelines(hash_renaming)
+                f_hash_def_one_renaming.writelines(hash_def_one_renaming)
+                f_hash_def_two_renaming.writelines(hash_def_two_renaming)
+                
+            writer.writerow([js_file_path, 'OK'])
+    
+        except Exception, e:
+            print js_file_path
             print str(e)
-            print
-            for r in result:
-                print r
             exit()
 #             writer.writerow([result[1], 'Writing failed'])
 
