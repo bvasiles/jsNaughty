a90e39e856d2a85224936678d3fced787d6cf773
Bogdan Vasilescu <vasilescu@gmail.com>
2016-07-08 11:01:18 -0700

    sampling

---
+++ experiments/preSample.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import random
+from unicodeManager import UnicodeReader, UnicodeWriter 
+
+file_in = os.path.abspath(sys.argv[1])
+file_out = os.path.abspath(sys.argv[2])
+size = int(sys.argv[3])
+
+data = []
+reader = UnicodeReader(open(file_in, 'r'))
+for row in reader:
+    data.append(row[0])
+
+data_sample = random.sample(data, size)
+
+with open(file_out, 'w') as of:
+    writer = UnicodeWriter(of)
+    for f in data_sample:
+        writer.writerow([f])
+ 
