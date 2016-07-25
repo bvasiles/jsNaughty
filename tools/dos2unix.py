9c722b088aa688c939ebc52f4b0e4408d9163939
Bogdan Vasilescu <vasilescu@gmail.com>
2016-07-25 13:41:57 -0700

    mass dos2unix

---
+++ tools/dos2unix.py
@@ -0,0 +1,25 @@
+import subprocess
+PIPE = subprocess.PIPE
+
+
+class Dos2Unix:
+    
+    def __init__(self, path=None):
+        if path is None:
+            self.path = '/usr/bin/dos2unix'
+        else:
+            self.path = path
+        
+        
+    def run(self, in_file_path):
+        ok = False
+        
+        # Call dos2unix
+        command = [self.path, in_file_path]
+        proc = subprocess.Popen(command, stderr=PIPE, stdout=PIPE)
+        _pc = proc.communicate()
+    
+        if not proc.returncode:
+            ok = True
+    
+        return ok
