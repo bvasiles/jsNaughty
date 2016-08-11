1cc66ccb8ccc2be5b3575b493944617284143f33
Bogdan Vasilescu <vasilescu@gmail.com>
2016-08-11 14:54:15 -0700

    trim

--- experiments/trimLongLines.py
+++ experiments/trimLongLines.py
@@ -1,5 +1,7 @@
 import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 
+                                             os.path.pardir)))
 import glob
 from folderManager import Folder
 
