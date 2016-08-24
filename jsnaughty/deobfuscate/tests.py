4d91fc148feb5c087c5838d90b78fd1005bde332
Casey Casalnuovo <caseycas@campus-110-149.ucdavis.edu>
2016-08-24 11:41:51 -0700

    Updating site code.

--- jsnaughty/deobfuscate/tests.py
+++ jsnaughty/deobfuscate/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from deobfuscate.experiments import MosesClient
 
 # Create your tests here.
+
+class DeobsfuscateWebTests(TestCase):
+    
+    def test_inner_deobsfucation(self):
+        #Subject to change once we have the postprocessing fixed.
+        rClient = MosesClient()
+        output = rClient.deobfuscateJS("var m = []", 0)
+        self.assertEqual(output.count("\n"), 9)
+        topSuggestion = output.split("\n")[0].split("|||")[1].strip()
+        self.assertEqual(topSuggestion, "var m = [ ] ;")
+        
