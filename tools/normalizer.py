18019cf49cec34f3ff197c4264c54f9319b958ec
Bogdan Vasilescu <vasilescu@gmail.com>
2016-08-26 16:13:24 -0700

    will do some prepro separately and save to disk

---
+++ tools/normalizer.py
@@ -0,0 +1,32 @@
+import subprocess
+PIPE = subprocess.PIPE
+
+
+class Normalizer:
+    
+    def __init__(self, normalizer_js_path=None):
+        if normalizer_js_path is None:
+            import os
+            self.normalizer_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'node_normalizer'))
+        else:
+            self.normalizer_dir = normalizer_js_path
+        
+        
+    def run(self, in_file_path, out_file_path=None):
+        ok = False
+        
+        command = ['node', 'normalize.js', in_file_path]
+        proc = subprocess.Popen(command, 
+                                stderr=PIPE, stdout=PIPE, 
+                                cwd=self.normalizer_dir)
+
+        out, err = proc.communicate()
+
+        if not proc.returncode:
+            ok = True
+            if out_file_path is not None:
+                with open(out_file_path, 'w') as f:
+                    f.write(out)
+    
+        return (ok, out, err)
+        
