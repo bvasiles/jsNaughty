b23aa16a703d4a90e7c78b1bc59b0422753d6d92
Bogdan Vasilescu <vasilescu@gmail.com>
2017-01-10 11:17:50 -0500

    x [ci_skip]

--- tools/helpers.py
+++ tools/helpers.py
@@ -33,17 +33,18 @@ def prepHelpers(iBuilder,
                 
                 if scopeAnalyst is not None:
                     name2defScope = scopeAnalyst.resolve_scope()
-                    isGlobal = scopeAnalyst.isGlobal
             
-#                     try:
-                    if not isGlobal.get((token, p), True):
+#                     isGlobal = scopeAnalyst.isGlobal
+#                     if not isGlobal.get((token, p), True):
+
+                    try:
                         def_scope = name2defScope[(token, p)]
                         
                         name_positions.setdefault((token, def_scope), [])
                         name_positions[(token, def_scope)].append((line_num, line_idx))
                         position_names[line_num][line_idx] = (token, def_scope)
-#                     except KeyError:
-#                         pass
+                    except KeyError:
+                        pass
 
                 else:
                     def_scope = None
