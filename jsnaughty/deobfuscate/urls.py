9ad494f9576890ef8b3c7af90b79233931847b3a
Casey Casalnuovo <caseycasal@gmail.com>
2017-01-10 15:38:09 -0800

    Changing URL direct to make the tool the home page.

--- jsnaughty/deobfuscate/urls.py
+++ jsnaughty/deobfuscate/urls.py
@@ -6,7 +6,9 @@ from . import views
 app_name = 'deobfuscate'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.get_js, name='get_js'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^team/$', views.team, name='team'),
     url(r'^results/$', views.results, name='results'),
     url(r'^get_js/$', views.get_js, name='get_js'),
 ]
