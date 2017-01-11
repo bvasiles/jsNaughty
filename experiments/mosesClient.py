52cb14aae83a5c82d3bbbcbde7972880e40568e9
Casey Casalnuovo <caseycasal@gmail.com>
2017-01-10 18:14:37 -0800

    Removing crashing hardcoded debug statements, adding in 4th test case file.

--- experiments/mosesClient.py
+++ experiments/mosesClient.py
@@ -283,18 +283,6 @@ class MosesClient():
         #proxyMap = self.splitTexts(lex_ugly.collapsedText, proxies)
         translation = ""
 
-        #Serial version
-        print("charPosition2Name----------------------------------------------------")
-        print(iBuilder_ugly.charPosition2Name)
-        print("name2CharPositions----------------------------------------------------")
-        print(iBuilder_ugly.name2CharPositions)
-        print("flatMap----------------------------------------------------")
-        print([iBuilder_ugly.flatMap[(line,col)] for (line, col) in iBuilder_ugly.name2CharPositions[u'e']])
-        print("revFlatMat----------------------------------------------------")
-        print(iBuilder_ugly.revFlatMat)
-        print("-------------------------------------------------------------")
-        print(lex_ugly.collapsedText)
-        print(renamedText)
         mosesParams["text"] = renamedText
         #mosesParams["text"] = lex_ugly.collapsedText
         mosesParams["align"] = "true"
@@ -337,6 +325,7 @@ class MosesClient():
         if translation is not None:
             # Parse moses output
             mp = MosesParser()
+            print(translation)
                 
             name_candidates = mp.parse(translation,
                                        a_iBuilder,
