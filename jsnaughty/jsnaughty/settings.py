564a6b81b41cd7e2298c01a079e87a98985100b8
Casey Casalnuovo <ccasal@ucdavis.edu>
2017-01-16 21:14:12 -0800

    Making sure settings are up to date.

--- jsnaughty/jsnaughty/settings.py
+++ jsnaughty/jsnaughty/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Third Party apps
-    'crispy_forms',
+    #Third Party Apps
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
