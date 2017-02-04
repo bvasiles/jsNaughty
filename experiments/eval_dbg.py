bdc9cdd4dd227dd38a8700857c74af8100d05366
Bogdan Vasilescu <vasilescu@gmail.com>
2017-02-04 17:59:17 -0500

    [ci skip]

--- experiments/eval_dbg.py
+++ experiments/eval_dbg.py
@@ -215,17 +215,17 @@ def processFile(l):
                                       iBuilder_ugly,
                                       scopeAnalyst)
             
-#             print 'After text:'
-#             print after_text
-#             print
+            print 'After text:'
+            print after_text
+            print
             
             (ok, beautified_after_text, _err) = clear.web_run(after_text)
             if not ok:
                 return (js_file_path, None, 'Beautifier fail')
             
-#             print 'Beautified:'
-#             print beautified_after_text
-#             print
+            print 'Beautified:'
+            print beautified_after_text
+            print
             
             # Save renamed input to disk for future inspection
             with open(temp_files['%s' % (r_strategy)], 'w') as f:
@@ -254,7 +254,7 @@ def processFile(l):
             print '\ntranslation-------------'
             print translation
             
-#             exit()
+            exit()
             
             (a_name_positions, 
              a_position_names,
