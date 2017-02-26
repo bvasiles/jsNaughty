d7b9058f6bf24b623717ba02edae63d9467ae116
Casey Casalnuovo <caseycasal@gmail.com>
2017-02-25 19:56:34 -0800

    Updating plot generation script.

--- evaluation/stats_final.r
+++ evaluation/stats_final.r
@@ -15,23 +15,23 @@ library(reshape2)
 stats_nomix <- read.csv("~/jsnaughty/evaluation/stats_nomix.csv", sep=";")
 stats_mix <- read.csv("~/jsnaughty/evaluation/stats_mix.csv", sep=";")
 
-#1)
+#1) (white background + shades.)
 boxplot(cbind(stats_nomix[,c("hash_def_one_renaming_freqlen", "hash_def_one_renaming_lm", 
                        "hash_def_one_renaming_logmodel", "no_renaming_freqlen", 
                        "no_renaming_lm", "no_renaming_logmodel")])/stats_nomix[,c("num_loc_names")], 
         las=2, ylab=paste("% names recovered -", nrow(stats_nomix), "files"))
 
 #ggplot version with colors!
-subset1 <- cbind(stats_nomix[,c("hash_def_one_renaming_freqlen", "hash_def_one_renaming_lm", 
-                      "hash_def_one_renaming_logmodel", "no_renaming_freqlen", 
-                      "no_renaming_lm", "no_renaming_logmodel")])/stats_nomix[,c("num_loc_names")]
-colnames(subset1) <- c("Freq (Hash)", "LM (Hash)", "Model (Hash)", "Freq", "LM", "Model")
+subset1 <- cbind(stats_nomix[,c("no_renaming_freqlen", "hash_def_one_renaming_freqlen", 
+                                "no_renaming_lm","hash_def_one_renaming_lm", 
+                                "no_renaming_logmodel", "hash_def_one_renaming_logmodel")])/stats_nomix[,c("num_loc_names")]
+colnames(subset1) <- c("Freq", "Freq (Hash)", "LM", "LM (Hash)", "Model", "Model (Hash)")
 #Want to melt the data so we have two labels (one for each of the 6), and one for colors.
 b_1 <- melt(subset1)
 
 b_1$Hashed <- b_1$variable == "Freq (Hash)" |  b_1$variable == "LM (Hash)"  |  b_1$variable == "Model (Hash)"
-c_plot <- ggplot(b_1, aes(x = variable, y = value, color = Hashed)) + geom_boxplot() + ggtitle("File level accuracy with and without hashes by selection technique") + xlab("Selection technique") + ylab(paste("% names recovered -", nrow(stats_nomix), "files"))
-ggsave(c_plot, file = "~/jsnaughty/evaluation/Plots/consistencyBoxplot.png")
+c_plot <- ggplot(b_1, aes(x = variable, y = value)) + geom_boxplot(aes(fill = Hashed)) +  scale_fill_brewer(palette="Blues") + theme_minimal() + ggtitle("File level accuracy with and without hashes by selection technique") + xlab("Selection technique") + ylab(paste("% names recovered -", nrow(stats_nomix), "files"))
+ggsave(c_plot, file = "~/jsnaughty/evaluation/Plots/consistencyBoxplot.pdf")
 
 #2)
 subset2 <- cbind(stats_mix[,c("hash_def_one_renaming_logmodel", "n2p")])/stats_mix[,c("num_loc_names")]
@@ -45,13 +45,17 @@ subset2 <- cbind(subset2[,c("file","Blended Model")])
 subset2 <- merge(subset3, subset2, by = c("file"))
 
 b_2 <- melt(subset2)
-mix_plot <- ggplot(b_2, aes(variable, y = value, color = variable))+ geom_boxplot() + ggtitle("File level accuracy for JSnice, Autonym, and Blended techniques") + xlab("Renaming technique") + ylab(paste("% names recovered -", nrow(stats_nomix), "files"))
-ggsave(mix_plot, file = "~/jsnaughty/evaluation/Plots/jsniceAndBlendBoxplot.png")
+mix_plot <- ggplot(b_2, aes(variable, y = value))+ geom_boxplot(aes(fill = variable)) +  scale_fill_brewer(palette="Blues") + ggtitle("File level accuracy for JSnice, Autonym, and Blended techniques") + xlab("Renaming technique") + ylab(paste("% names recovered -", nrow(stats_nomix), "files"))
+ggsave(mix_plot, file = "~/jsnaughty/evaluation/Plots/jsniceAndBlendBoxplot.pdf")
 
 #3)#Bogdan's start point:
-ggplot(data=stats,aes(hash_def_one_renaming_logmodel/num_loc_names,n2p/num_loc_names))+
-  geom_hex()+
-  xlab("Autonym")+
-  ylab("JSNice")+ 
-  geom_abline(slope = 1)
+non_zero <- subset3[subset3$Autonym != 0 & subset3$JSNice != 0,]
 
+hex_plot <- ggplot(data=non_zero,aes(Autonym,JSNice))+
+  geom_hex()+
+  xlab("Autonym File Accuracy")+
+  ylab("JSNice File Accuracy")+ 
+  geom_abline(slope = 1, size = 2)+
+  ggtitle("Comparison of Autonym vs JSNice Accuracy")
+
+ggsave(hex_plot, file = "~/jsnaughty/evaluation/Plots/hex_comparison_plot.pdf")
