5c3297fd2f6de8435f48a4e89ecc4ab1fa3f3eda
Casey Casalnuovo <caseycasal@gmail.com>
2017-04-17 16:47:57 -0700

    Fixing link to base webpage.

--- jsnaughty/jsnaughty/urls.py
+++ jsnaughty/jsnaughty/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    #url(r'^$', RedirectView.as_view(url = reverse_lazy('/deobfuscate'), permanent=False)),
+    url(r'^', include('deobfuscate.urls')),
     url(r'^deobfuscate/', include('deobfuscate.urls')),
     url(r'^admin/', admin.site.urls),
 ]
