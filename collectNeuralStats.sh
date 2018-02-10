d0519ea7974e5bd2ae3be3363c8cd5dc6ee484e2
Casey Casalnuovo <ccasal@ucdavis.edu>
2018-02-09 16:37:51 -0800

    Adding shell scripts.

--- collectNeuralStats.sh
+++ collectNeuralStats.sh
@@ -6,10 +6,10 @@ python experiments/testStatsNoSanity.py experiments/results/test_rep_moses_1_per
 
 python experiments/testStatsNoSanity.py experiments/results/test_rep_moses_5_percent/candidates.csv experiments/results/test_rep_moses_5_percent/ experiments/results/test_rep_moses_5_percent/log_test_test_rep_moses_5_percent 1 1
 
-#python experiments/testStatsNoSanity.py experiments/results/test_rep_neural_5_percent/candidates.csv experiments/results/test_rep_neural_5_percent/ experiments/results/test_rep_neural_5_percent/log_test_test_rep_neural_5_percent 1 1
+python experiments/testStatsNoSanity.py experiments/results/test_rep_neural_5_percent/candidates.csv experiments/results/test_rep_neural_5_percent/ experiments/results/test_rep_neural_5_percent/log_test_test_rep_neural_5_percent 1 1
 
 cp experiments/results/test_replication_fix/stats.csv evaluation/neural_stats_fix_approx.csv
 cp experiments/results/test_replication_keep_same/stats.csv evaluation/neural_stats_ks_approx.csv
 cp experiments/results/test_rep_moses_1_percent/stats.csv evaluation/stats_moses_1_percent_approx.csv
 cp experiments/results/test_rep_moses_5_percent/stats.csv evaluation/stats_moses_5_percent_approx.csv
-#cp experiments/results/test_rep_neural_5_percent/stats.csv evaluation/stats_neural_5_percent_approx.csv
+cp experiments/results/test_rep_neural_5_percent/stats.csv evaluation/stats_neural_5_percent_approx.csv
