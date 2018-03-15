8f94ea5c889aec857269067e0248417e34114efa
Neil Xu <xzy@cmu.edu>
2018-03-15 18:59:29 +0000

    Some changes

--- runMoses1PercentTest.sh
+++ runMoses1PercentTest.sh
@@ -1,17 +1,17 @@
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 0 -stop 250 --reset
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/ -start 0 -stop 250 --reset
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 251 -stop 500
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/  -start 251 -stop 500
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 501 -stop 750
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/  -start 501 -stop 750
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 751 -stop 1000
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/  -start 751 -stop 1000
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 1001 -stop 1250
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/  -start 1001 -stop 1250
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 1251 -stop 1500
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/  -start 1251 -stop 1500
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 1501 -stop 1750
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/  -start 1501 -stop 1750
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 1750 -stop 2000
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/ -start 1750 -stop 2000
 
-python experiments/runNeuralTestCorpus.py -s experiments/results/test_rep_moses_1_percent/ -o experiments/results/test_rep_moses_1_percent/ -start 2001 -stop 2150
+python experiments/runNeuralTestCorpus.py -s experiments/results/jsnaughty_test_set/ -o experiments/results/jsnaughty_test_set/2018-03-01/  -start 2001 -stop 2150
